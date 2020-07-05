a8fe6ee2f364b48cd389bfa2433dcdd7e729c4e0
Geethu Radhakrishnan <v-gradhakrishnan@carrentals.com>
2020-07-06 00:34:42 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-import Home from './components/Home/Home';
 import Footer from './components/Footer/Footer';
 
 function App() {
